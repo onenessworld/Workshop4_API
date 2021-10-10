e77f2402b5bab97efbf09b2a12a8eb7cb6bc3353
Oneness World <mgokulan@andrew.cmu.edu>
2021-10-09 20:42:38 -0700

    Added extra comments.

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ class HelloWorld(Resource):
 # Resource supporting GET method with parameters.
 class SampleResource(Resource):
     def get(self):
-        get_parser = reqparse.RequestParser()
-        get_parser.add_argument('sample_param', type=str)
-        args = get_parser.parse_args()
+        get_parser = reqparse.RequestParser()  # From flask library.
+        get_parser.add_argument('sample_param', type=str)  # Expect a query param called "sample_param".
+        args = get_parser.parse_args()  # Stores value of sample param, defaults to None.
         sample_response = "GET parameter *sample_param* got value: *%s*" % args.sample_param
         return sample_response
 
@@ -24,11 +24,9 @@ class SampleResource(Resource):
 # Resource supporting GET method with optional resource ID.
 class AnotherResource(Resource):
     def get(self, resource_id=None):
-        # If resource ID is provided, GET matching resource.
-        if resource_id:
+        if resource_id:  # If resource ID is provided, GET matching resource.
             sample_response = "GET with resource ID: *%s*" % resource_id
-        # Else, print an error or GET all resources.
-        else:
+        else:  # Else, print an error or GET all resources.
             sample_response = "GET with no resource ID: this is either an error, or an opportunity to display all " \
                               "available resource. "
         return sample_response
@@ -42,9 +40,9 @@ api.add_resource(HelloWorld, '/')
 api.add_resource(SampleResource, '/sample_route')
 
 # http://localhost:5000/another_route
-# http://localhost:5000/another_route/some_id
+# http://localhost:5000/another_route/some_resource_id
 api.add_resource(AnotherResource, '/another_route', '/another_route/<string:resource_id>')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run()  # Runs web app @ http://localhost:5000 by default for me.
